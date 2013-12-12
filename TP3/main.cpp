abae1bd51e04d1503c530396eae2bd026ea7ccf1
Cybil Bourely <cybiltech@gmail.com>
2013-12-12 17:10:44 -0500

    add test file && flags g++

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -1,11 +1,18 @@
 
+#include <iostream>
+#include <fstream>
 #include "Adresse.h"
 #include "Annuaire.h"
 
 int		main()
 {
-  Adresse		adr(62, "rue des Bourrelier", "Moissy_Cramayel");
-  Annuaire	anu;
+  std::ifstream	fichier("test.txt", std::ios::in);
+  
+  if (!fichier)
+    return -1;
+  // Adresse		adr(62, "rue des Bourrelier", "Moissy_Cramayel");
+  Annuaire	anu(fichier);
 
+  fichier.close();
   return 0;
 }
