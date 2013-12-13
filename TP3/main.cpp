a96ab2c77ef988819e93acac9250dd94fa08a47c
Mr21 <thomastortorini@gmail.com>
2013-12-13 14:40:30 -0500

    main

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -4,10 +4,9 @@
 #include "Annuaire.h"
 #include "ArbreAVL.h"
 
-int		main()
-{
+int		main() {
 //  std::ifstream	fichier("test.txt", std::ios::in);
-  
+
 //  if (!fichier)
 //    return -1;
 //  // Adresse		adr(62, "rue des Bourrelier", "Moissy_Cramayel");
@@ -17,18 +16,22 @@ int		main()
 //  std::cout << anu;
 //  fichier.close();
 //
-  ArbreAVL<int, int>	abr;
-  std::cout << abr.appartient(120) << std::endl;
-  abr.inserer(1, 1);
-  std::cout << abr.appartient(1) << std::endl;
-  abr.lister();
-//  abr.inserer(3, 3);
+    ArbreAVL<int, char*>	abr;
+    //std::cout << abr.appartient(120) << std::endl;
+    //abr.inserer(3, 3);
+    //std::cout << abr.appartient(1) << std::endl;
+    abr.inserer(5, "a");
+    abr.show();
+    std::cout << "---" << std::endl;
+    abr.inserer(2, "b");
+    abr.inserer(4, "c");
+    abr.inserer(3, "d");
+    abr.show();
 //  abr.inserer(4, 4);
 //  abr.inserer(5, 5);
 //  std::cout << abr.appartient(2) << std::endl;
 //  std::cout << abr.appartient(3) << std::endl;
 //  std::cout << abr.appartient(4) << std::endl;
 //  std::cout << abr.appartient(5) << std::endl;
-  std::cout << "insertion" << std::endl;
   return 0;
 }
